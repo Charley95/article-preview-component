18dd26f7ea6e78918034d662612c4d1d1f5b7d0c
Chinenye <ibenyenwachinenye07@gmail.com>
2025-04-13 18:10:25 +0100

    added more functionality to my share button

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
-const shareBtn = document.getElementsByClassName("share")[0];
-const activeInfo = document.getElementsByClassName("info")[0];
-const socialIcons = document.getElementsByClassName('social-media-icons')[0];
+const shareBtn = document.querySelector("#share");
+const activeInfo = document.querySelector(".info");
+const socialIcons = document.querySelector('.social-media-icons');
+const shareBtnAlt = document.querySelector(".share-alt");
+const tabletActiveInfo = document.querySelector(".share-tablet");
+const shareCircle = document.querySelector(".share-circle");
+const svgIcon = document.querySelector(".share-circle svg");
 
-shareBtn.addEventListener("click", function() {
+shareBtn.addEventListener("click", function () {
+  if (window.innerWidth <= 768) {
     activeInfo.classList.toggle("hidden");
     socialIcons.classList.toggle("hidden");
+  } else {
+    tabletActiveInfo.classList.toggle("hidden");
+    shareCircle.classList.toggle("circle-large");
+  }
 });
 
-const shareBtnAlt = document.getElementsByClassName("share-alt")[0];
+if (shareBtnAlt) {
+  shareBtnAlt.addEventListener("click", function () {
+    activeInfo.classList.toggle("hidden");
+    socialIcons.classList.toggle("hidden");
+  });
+}
 
-shareBtnAlt.addEventListener("click", function () {
-  activeInfo.classList.toggle("hidden");
-  socialIcons.classList.toggle("hidden");
+window.addEventListener("resize", function () {
+  tabletActiveInfo.classList.add("hidden");
+  socialIcons.classList.add("hidden");
 });
+
